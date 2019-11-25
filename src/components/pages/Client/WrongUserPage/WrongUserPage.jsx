c5a02854a346d5c37584c935b7b2881cd7aa6198
Alexander Mikhaylov <aleksandr.mikhaylov@onetwotrip.com>
2019-11-25 22:12:30 +0300

    add page to vcs

---
+++ src/components/pages/Client/WrongUserPage/WrongUserPage.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div
+      style={{
+        padding: '50px',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <h1>404</h1>
+      <p>Вы зашли по неправильному адресу</p>
+    </div>
+  );
+};
