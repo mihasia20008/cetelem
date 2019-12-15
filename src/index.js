7984fed272d0b69c21dd1955ffcc2bbe8ece1804
Alexander Mikhaylov <aleksandr.mikhaylov@onetwotrip.com>
2019-12-15 17:15:44 +0300

    fix promise

--- src/index.js
+++ src/index.js
@@ -10,6 +10,23 @@ import store from './redux/configureStore';
 import history from './utilities/history';
 import { LayoutContextProvider } from './utilities/layoutContext';
 
+// /**
+//  * Turns an array of promises into a promise for an array of their states (as
+//  * returned by `inspect`) when they have all settled.
+//  * @param {Array[Any*]} values an array (or promise for an array) of values (or
+//  * promises for values)
+//  * @returns {Array[State]} an array of states for the respective values.
+//  */
+// Promise.prototype.allSettled = function () {
+//   return this.then((promises) => {
+//     const wrappedPromises = promises.map(p => Promise.resolve(p)
+//       .then(
+//         val => ({ status: 'fulfilled', value: val }),
+//         err => ({ status: 'rejected', reason: err })));
+//     return Promise.all(wrappedPromises);
+//   });
+// };
+
 render(
   <Provider store={store}>
     <LayoutContextProvider>
