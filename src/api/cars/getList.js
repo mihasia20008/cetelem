8b23677f29c3f3aedff5a19abf3bf9b6e083ea15
Alexander Mikhaylov <aleksandr.mikhaylov@onetwotrip.com>
2019-12-22 18:24:44 +0300

    move cars list params logic to redux action

--- src/api/cars/getList.js
+++ src/api/cars/getList.js
@@ -1,17 +1,11 @@
 import axios from 'axios';
 import qs from 'qs';
-import _pick from 'lodash/pick';
 import _get from 'lodash/get';
-
-import { FILTER_TYPES, FILTER_NAMES } from '../../constants';
 
 const page = 'x-page';
 const pagesCount = 'x-pages-number';
 const perPage = 'x-per-page';
 const total = 'x-total';
-
-const availableParams = ['page', 'per_page', 'sort', 'order'];
-const filtersParams = Object.values(FILTER_NAMES);
 
 function prepareCars(list) {
   return list.map(item => {
@@ -25,59 +19,11 @@ function prepareCars(list) {
   });
 }
 
-export async function carListRequest(params, filters) {
+export async function carListRequest(params) {
   try {
-    const filtersQuery = _pick(params, filtersParams);
-    const preparedFilters = Object.keys(filtersQuery).reduce((acc, key) => {
-      if (key.search('_id') !== -1 && filtersQuery[key] > 0) {
-        acc[key] = filtersQuery[key];
-        return acc;
-      }
-
-      const filter = filters[key];
-      if (!filter) {
-        return acc;
-      }
-
-      switch (filter.type) {
-        case FILTER_TYPES.CHECKBOX: {
-          acc[key] = filtersQuery[key];
-          break;
-        }
-        case FILTER_TYPES.SELECT: {
-          const option = filter.options.find(
-            item => item.id.toString() === filtersQuery[key].toString()
-          );
-          if (option) {
-            acc[key] = option.name;
-          }
-          break;
-        }
-        case FILTER_TYPES.RANGE: {
-          if (Array.isArray(filtersQuery[key])) {
-            const [min, max] = filtersQuery[key];
-            acc[`${key}[min]`] = min;
-            acc[`${key}[max]`] = max;
-          } else {
-            acc[`${key}[min]`] = filtersQuery[key];
-          }
-          break;
-        }
-        default: {
-          break;
-        }
-      }
-      return acc;
-    }, {});
-
-    if (!(FILTER_NAMES.NEW in preparedFilters)) {
-      preparedFilters[FILTER_NAMES.NEW] = 1;
-    }
-
-    const baseQuery = _pick(params, availableParams);
     const { headers, data } = await axios({
       method: 'GET',
-      url: `/api/v1/dealer_cars?${qs.stringify({ ...baseQuery, ...preparedFilters })}`,
+      url: `/api/v1/dealer_cars?${qs.stringify(params)}`,
     });
 
     return {
--- src/redux/modules/cars/actions.js
+++ src/redux/modules/cars/actions.js
@@ -1,13 +1,64 @@
+import _pick from 'lodash/pick';
+
 import { carsRequests } from '../../../api';
 
 import * as T from './types';
+import { FILTER_NAMES, FILTER_TYPES } from '../../../constants';
+
+const availableParams = ['page', 'per_page', 'sort', 'order'];
+const filtersParams = Object.values(FILTER_NAMES);
 
 export function loadCarsList(query) {
   return async (dispatch, getStore) => {
     try {
-      dispatch({ type: T.CARS_FETCH_START});
+      dispatch({ type: T.CARS_FETCH_START });
       const filters = getStore().filters.data;
-      const { error, data } = await carsRequests.loadCarsList(query, filters);
+      const filtersQuery = _pick(query, filtersParams);
+      const preparedFilters = Object.keys(filtersQuery).reduce((acc, key) => {
+        if (key.search('_id') !== -1 && filtersQuery[key] > 0) {
+          acc[key] = filtersQuery[key];
+          return acc;
+        }
+
+        const filter = filters[key];
+        if (!filter) {
+          return acc;
+        }
+
+        switch (filter.type) {
+          case FILTER_TYPES.CHECKBOX: {
+            acc[key] = filtersQuery[key];
+            break;
+          }
+          case FILTER_TYPES.SELECT: {
+            const option = filter.options.find(
+              item => item.id.toString() === filtersQuery[key].toString()
+            );
+            if (option) {
+              acc[key] = option.name;
+            }
+            break;
+          }
+          case FILTER_TYPES.RANGE: {
+            if (Array.isArray(filtersQuery[key])) {
+              const [min, max] = filtersQuery[key];
+              acc[`${key}[min]`] = min;
+              acc[`${key}[max]`] = max;
+            } else {
+              acc[`${key}[min]`] = filtersQuery[key];
+            }
+            break;
+          }
+          default: {
+            break;
+          }
+        }
+        return acc;
+      }, {});
+
+      const baseQuery = _pick(query, availableParams);
+
+      const { error, data } = await carsRequests.loadCarsList({ ...baseQuery, ...preparedFilters });
 
       if (error) {
         dispatch({ type: T.CARS_FETCH_ERROR, data: error });
@@ -18,7 +69,10 @@ export function loadCarsList(query) {
     } catch (error) {
       // eslint-disable-next-line no-console
       console.log(error);
-      dispatch({ type: T.CARS_FETCH_ERROR, data: { message: 'Ошибка получения списка автомобилей' } });
+      dispatch({
+        type: T.CARS_FETCH_ERROR,
+        data: { message: 'Ошибка получения списка автомобилей' },
+      });
     } finally {
       dispatch({ type: T.CARS_FETCH_END });
     }
